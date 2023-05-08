8d5a48644c0c41c0a53c87d8952729ea1751cb09
Andreas Nyström <andreas.o.nystrom+GitHub@gmail.com>
2023-05-08 10:20:34 +0200

    Test

---
+++ src/test/java/org/example/pages/BasePage.java
@@ -0,0 +1,8 @@
+package org.example.pages;
+
+public class BasePage {
+
+    public void test(){
+        System.out.println("hej");
+    }
+}
---
+++ src/test/java/org/example/selenium/BaseTest.java
@@ -0,0 +1,9 @@
+package org.example.selenium;
+
+public class BaseTest {
+
+    public void test(){
+        System.out.println("hej");
+    }
+
+}
