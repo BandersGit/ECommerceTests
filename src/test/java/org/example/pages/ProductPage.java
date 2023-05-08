50182fa17b7dcef747decabb6c3bacd0e3d5eea4
Steven <stevenbergqvist@gmail.com>
2023-05-08 14:07:28 +0200

    product page buy button

--- src/test/java/org/example/pages/ProductPage.java
+++ src/test/java/org/example/pages/ProductPage.java
@@ -1,4 +1,12 @@
 package org.example.pages;
 
-public class ProductPage {
+import org.openqa.selenium.By;
+
+public class ProductPage extends BasePage{
+
+    private final By buyNowButton = By.linkText("Buy now");
+
+    public void clickBuyNowButton(){
+        click(buyNowButton);
+    }
 }
