528339d38957ba105a0994c48cd2b38fc5699372
SiddhiQi <kim.siddiqi@gmail.com>
2023-05-08 13:53:16 +0200

    follow link methods

--- src/test/java/org/example/pages/LandingPage.java
+++ src/test/java/org/example/pages/LandingPage.java
@@ -1,4 +1,19 @@
 package org.example.pages;
 
+import org.openqa.selenium.By;
+
 public class LandingPage extends BasePage {
+
+    private final By loginPageLink = By.linkText("My account");
+    private final By registerPageLink = By.linkText("Register");
+
+    public LoginPage followLinkToLoginPage (){
+        click(loginPageLink);
+        return new LoginPage();
+    }
+
+    public RegisterPage followLinkToRegisterPage(){
+        click(registerPageLink);
+        return new RegisterPage();
+    }
 }
