4d1296029408645628a9fba9d23d02ab19455e73
Steven <stevenbergqvist@gmail.com>
2023-05-08 14:55:36 +0200

    Checkoutpage

--- src/test/java/org/example/pages/CheckoutPage.java
+++ src/test/java/org/example/pages/CheckoutPage.java
@@ -1,4 +1,28 @@
 package org.example.pages;
 
-public class CheckoutPage {
+import org.openqa.selenium.By;
+
+public class CheckoutPage extends BasePage {
+
+    private final By firstNameField = By.id("input-payment-firstname");
+    private final By lastNameField = By.id("input-payment-lastname");
+    private final By addressOneField = By.id("input-payment-address-1");
+    private final By cityField = By.id("input-payment-city");
+    private final By postCodeField = By.id("input-payment-postcode");
+
+    private final By checkAgreeTermsAndConditions = By.id("input-agree");
+
+    private final By buttonContinue = By.id("button-save");
+
+    public void typeIntoFirstNameField(String input){
+        sendKeys(firstNameField, input);
+    }
+    //FORTSÄTT MED INPUTS HÄR
+    public void clickCheckAgreeTermsAndConditions(){
+        click(checkAgreeTermsAndConditions);
+    }
+
+    public void clickButtonContinue(){
+        click(buttonContinue);
+    }
 }
