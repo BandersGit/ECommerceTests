258ab4a6b3391fbb3035587eeee6244bebd665cf
SiddhiQi <kim.siddiqi@gmail.com>
2023-05-09 10:59:38 +0200

    Login refactor...

--- src/test/java/org/example/pages/LoginPage.java
+++ src/test/java/org/example/pages/LoginPage.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.By;
 
 public class LoginPage extends BasePage{
 
-    private final By emailAddressInputField = By.id("input-email");
+    private final By emailInputField = By.id("input-email");
     private final By passwordInputField = By.id("input-password");
     private final By loginButton = By.xpath("//*[@id=\"content\"]/div/div[2]/div/div/form/input");
 
-    public void enterEmailAddress(String emailAddress){
-        sendKeys(emailAddressInputField, emailAddress);
+    public void enterEmail(String email){
+        sendKeys(emailInputField, email);
     }
 
     public void enterPassword(String password){
--- src/test/java/org/example/selenium/LoginPageTest.java
+++ src/test/java/org/example/selenium/LoginPageTest.java
@@ -5,21 +5,15 @@ import org.testng.annotations.Test;
 
 public class LoginPageTest extends BaseTest {
 
-    //test follow link to Login Page
-    @Test
-     public void login() {
-        LoginPage loginPage = landingPage.followLinkToLoginPage();
-    }
-
     @Test
     public void login_whenInputValidAccountDetails_shouldLogin(){
         //Given
-        String emailAddress = "aName";
+        String email = "aName";
         String password = "aPassword";
 
         //When
         LoginPage loginPage = landingPage.followLinkToLoginPage();
-        loginPage.enterEmailAddress(emailAddress);
+        loginPage.enterEmail(email);
         loginPage.enterPassword(password);
         loginPage.clickLoginButton();
 
